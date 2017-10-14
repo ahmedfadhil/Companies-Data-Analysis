7fa83419e0fa0fac6aa08beeaed525bd35326e35
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-10-14 22:22:24 +0200

    plotted the data into a heatmap

--- companiesanddata.py
+++ companiesanddata.py
@@ -5,6 +5,11 @@ import pandas as pd
 import pandas_datareader.data as web
 import os
 import requests
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
 
 
 def save_sp500_tickers():
@@ -76,3 +81,36 @@ def compile_data():
     main_df.to_csv('sp500_closes.csv')
 
     compile_data()
+
+
+def visualize_data():
+    df = pd.read_csv('sp500_closes.csv')
+    # df['AAPL'].plot()
+    # plt.show()
+    df_corr = df.corr()
+
+    print(df.corr().head())
+
+    data = df_corr.values
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    heatmap = ax.pcolor(data, cmap=plt.cm.RdYlGn)
+    fig.colorbar(heatmap)
+    ax.set_xticks(np.arange(data.shape[0] + 0.5, minor=False))
+    ax.set_yticks(np.arange(data.shape[1] + 0.5, minor=False))
+
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    column_labels = df_corr.columns()
+    row_labels = df_corr.index()
+
+    ax.set_xticklabels(column_labels)
+    ax.set_yticklabels(row_labels)
+    plt.xticks(rotation = 90)
+    heatmap.set_clim(-1,1)
+    plt.tight_layout()
+    plt.show()
+
+
+visualize_data()
